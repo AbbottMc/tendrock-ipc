d5f8a9f796a758a20c963539ed546938ef984747
QianShanyao <qian_shan_yao@163.com>
2023-04-07 22:04:54 +0800

    移除测试代码

--- Main.ts
+++ Main.ts
@@ -2,8 +2,3 @@ import {Repeater} from './tenon/core/Repeater'
 
 export {RepeaterSystem} from './tenon/core/RepeaterSystem';
 export const repeater = new Repeater();
-
-const repeaterSystem = repeater.register('cs');
-repeaterSystem.monit(({value, identifier})=>{
-
-});
